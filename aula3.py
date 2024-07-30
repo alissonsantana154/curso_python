ac40580ae7a75b790c78151eb5c7452b85350d78
Alisson Santana <alissonsantana16608@gmail.com>
2024-07-30 20:39:02 -0300

    Tipo str

---
+++ aula3.py
@@ -0,0 +1,23 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Álisson santana')
+print(1, 'Álisson "santana"')
+
+
+# Aspas Duplas 
+print("Álisson santana")
+print(2, "Álisson 'santana'")
+
+# Escape
+print("Álisson \"santana\"")
+
+# r 
+print(r"Álisson \"santana\"")
+
