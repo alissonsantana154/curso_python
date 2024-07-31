a9777afdb7bb4ec55bf3f110671e3c64f7fcf7e4
Alisson Santana <alissonsantana16608@gmail.com>
2024-07-31 20:53:05 -0300

    if, elif e else

---
+++ aula16.py
@@ -0,0 +1,16 @@
+# if / elif       / else
+# se / se não se / se não 
+
+entrada = input('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+  print('Você entrou com sucesso!')
+
+  print(12341234)
+elif entrada == 'sair':
+  print('Você saiu com sucesso!')
+  print('Até mais!')
+else:
+  print('Entrada inválida!')
+
+print('FORA DOS BLOCOS')
