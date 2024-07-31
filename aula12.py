22a5b54054c68e5df2220c385b2bc2490c30d6c3
Alisson Santana <alissonsantana16608@gmail.com>
2024-07-31 19:18:19 -0300

    Operadores aritméticos

---
+++ aula12.py
@@ -0,0 +1,6 @@
+nome = 'Alisson Santana'
+altura = 1.69
+peso = 87
+imc = peso / (altura * altura)
+
+print('{} tem {} de altura, \n pesa {} quilos e seu IMC é \n {}'.format(nome, altura, peso, imc))
---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Álisson'
+sobrenome = 'Santana'
+idade = 22
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.69
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('Maior de idade: ', maior_de_idade)
+print('Altura Em metros: ', altura_metros)
