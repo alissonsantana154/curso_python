db79ce7ca20a79497e05a6dcdbfc318ef46d2fab
Alisson Santana <alissonsantana16608@gmail.com>
2024-08-13 19:30:13 -0300

    Retorno de valores das funções (return)

---
+++ aula70.py
@@ -0,0 +1,16 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma(x, y):
+    if x > 10 :
+        return[10, 20]
+    return x + y
+
+# variavel1 = soma(1, 2)
+# variavel2 = soma('1')
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1)
+print(soma2)
+print(soma(11, 55))
